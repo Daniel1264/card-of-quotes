434fa59b7064dc7e526775de08c46970c86c426e
Daniel1264 <ramosperca015@gmail.com>
2022-09-16 12:38:16 -0500

    second update (icon quote from boxicon)

--- src/components/QuoteBox.jsx
+++ src/components/QuoteBox.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Button from './Button'
+// import 'boxicons'
 
 const QuoteBox = ({random, randomColor, firstAll}) => {
     
@@ -7,9 +8,11 @@ const QuoteBox = ({random, randomColor, firstAll}) => {
         color: randomColor
     }
   return (
-    <article style={colorObj}>
-      <p>{random.quote}</p>
-      <h2>{random.author}</h2>
+    <article style={colorObj} className='card'>
+    <link href='https://unpkg.com/boxicons@2.1.2/css/boxicons.min.css' rel='stylesheet'></link>
+
+      <p className='text_quote'><i class='bx bxs-quote-alt-left'></i>{random.quote}</p>
+      <h2 className='author_quote'>{random.author}</h2>
       <Button 
       firstAll = {firstAll}
       randomColor = {randomColor}
